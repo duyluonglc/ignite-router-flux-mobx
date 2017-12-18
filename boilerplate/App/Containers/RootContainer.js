66bcc8a9d3175e4c5fcb1226bc268535d408e9a7
Thien <cvt3894@gmail.com>
2017-12-18 17:35:06 +0700

    update

--- boilerplate/App/Containers/RootContainer.js
+++ boilerplate/App/Containers/RootContainer.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react'
 import { View, StatusBar } from 'react-native'
 import AppNavigation from '../Navigation/AppNavigation'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
 // Styles
 import styles from './Styles/RootContainerStyles'
 import { Metrics } from '../Themes'
 
 class RootContainer extends Component {
   componentDidMount () {
-    // if redux persist is not active fire startup action
-    if (!ReduxPersist.active) {
-      this.props.startup()
-    }
+    this.props.startup()
   }
 
   render () {
@@ -32,7 +27,7 @@ class RootContainer extends Component {
 
 // wraps dispatch to create nicer functions to call within our component
 const mapDispatchToProps = (dispatch) => ({
-  startup: bindActionCreators(StartupActions.startup, dispatch)
+  startup: () => dispatch(StartupActions.startup())
 })
 
 export default connect(null, mapDispatchToProps)(RootContainer)
